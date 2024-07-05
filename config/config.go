027e902232c33c67b9d027f8d4a5ff2d437f7bf1
Hundera Awoke <hunderaweke@gmail.com>
2024-07-05 15:00:12 +0300

    WIP: global and local user config

--- config/config.go
+++ config/config.go
@@ -1,81 +1,46 @@
 package config
 
 import (
-	"bytes"
 	"encoding/json"
-	"io"
-	"io/ioutil"
 	"os"
-	"path/filepath"
 
-	"github.com/fatih/color"
+	"github.com/mitchellh/go-homedir"
 )
 
-type CodeTemplate struct {
-	Alias        string `json:"alias,omitempty"`
-	Lang         string `json:"lang,omitempty"`
-	Path         string `json:"path,omitempty"`
-	Suffix       string `json:"suffix,omitempty"`
-	BeforeScript string `json:"before_script,omitempty"`
-	Script       string `json:"script,omitempty"`
-	AfterScript  string `json:"after_script,omitempty"`
+type Config interface {
+	save() error
 }
-type Config struct {
-	Templates       []CodeTemplate    `json:"templates,omitempty"`
-	DefaultTemplate int               `json:"default_template,omitempty"`
-	GenAfterParse   bool              `json:"gen_after_parse,omitempty"`
-	Host            string            `json:"host,omitempty"`
-	Proxy           string            `json:"proxy,omitempty"`
-	FolderName      map[string]string `json:"folder_name,omitempty"`
-	path            string
+type LocalConfig struct {
+	Lang       string `json:"lang,omitempty"`
+	TemplateId int    `json:"template_id,omitempty"`
+	ContestId  string `json:"contest_id,omitempty"`
+}
+type GlobalConfig struct {
+	Handle    string     `json:"handle,omitempty"`
+	Host      string     `json:"host,omitempty"`
+	Templates []Template `json:"templates,omitempty"`
 }
 
-var Instance *Config
-
-func Init(path string) {
-	c := &Config{path: path, Host: "https://codeforces.com", Proxy: ""}
-	if err := c.load(); err != nil {
-		color.Green("Creating a new configuration in %v \n", path)
-	}
-	if c.DefaultTemplate < 0 || c.DefaultTemplate >= len(c.Templates) {
-		c.DefaultTemplate = 0
-	}
-	if c.FolderName == nil {
-		c.FolderName = map[string]string{}
-	}
-	if _, ok := c.FolderName["root"]; !ok {
-		c.FolderName["root"] = ".codative_config"
-	}
-	c.save()
-	Instance = c
+func load() (Config, error) {
+	//TODO: Loading both Global and Local Config
 
 }
 
-func (c *Config) load() (err error) {
-	file, err := os.Open(c.path)
+func (c *LocalConfig) save() error {
+	//TODO: Saving the local config to the Contest path
+	return nil
+}
+func (c *GlobalConfig) save() error {
+	homeDir, err := homedir.Dir()
 	if err != nil {
-		return
+		return err
 	}
-	defer file.Close()
-	bytes, err := io.ReadAll(file)
-	if err != nil {
-		return
-	}
-	return json.Unmarshal(bytes, c)
-}
-
-func (c *Config) save() (err error) {
-	var data bytes.Buffer
-	encoder := json.NewEncoder(&data)
-	encoder.SetIndent("", "  ")
-	encoder.SetEscapeHTML(false)
-	err = encoder.Encode(c)
+	_, err = os.ReadFile(homeDir + ".codative_config")
 	if err == nil {
-		os.MkdirAll(filepath.Dir(c.path), os.ModePerm)
-		err = ioutil.WriteFile(c.path, data.Bytes(), 0644)
+		os.Remove(homeDir + ".codative_config")
 	}
-	if err != nil {
-		color.Red("Cannot Save file in the directory %v\n%v", c.path, err.Error())
-	}
-	return
+	file, err := os.Create(homeDir + ".codative_config")
+	bytes, err := json.Marshal(c)
+	file.Write(bytes)
+	return nil
 }
