e22b2e34ac972ab95b6387b3cc7733c329fae0ac
Hundera Awoke <hunderaweke@gmail.com>
2024-07-26 13:24:24 +0300

    updated: template creating prompt

--- internal/template.go
+++ internal/template.go
@@ -2,7 +2,9 @@ package internal
 
 import (
 	"os"
+	"strings"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/fatih/color"
 )
 
@@ -36,4 +38,40 @@ func (t *Template) createFile(fileName string) error {
 func NewTemplate(filePath, lang, alias string) Template {
 	t := Template{Path: filePath, Lang: lang, Alias: alias}
 	return t
+}
+func TemplatePrompt() (Template, error) {
+	var t Template
+	var options []string
+	for id, lang := range Langs {
+		options = append(options, id+" "+lang)
+	}
+	q := []*survey.Question{
+		{
+			Name: "path",
+			Prompt: &survey.Input{
+				Message: "Enter the template path (absolute path):",
+			},
+			Validate: survey.Required,
+		},
+		{
+			Name: "lang",
+			Prompt: &survey.Select{
+				Message: "Choose Language",
+				Options: options,
+				Help:    "This is the language that will be used while submitting",
+			},
+			Validate: survey.Required,
+		},
+		{
+			Name: "alias",
+			Prompt: &survey.Input{
+				Message: "Insert an alias for the template",
+				Help:    "This is the alias that will be used for loading and searching the alias",
+			},
+			Validate: survey.Required,
+		},
+	}
+	survey.Ask(q, &t)
+	t.Lang = strings.Split(t.Lang, " ")[0]
+	return t, nil
 }
